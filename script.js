2145b0ffdca20e1a663be22554a61655edad9685
Dexter Logdonio <zerosevenboys@gmail.com>
2023-12-30 15:01:21 +0800

    Added fruits to elements 12/30/23

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-var elementDisplay = (buttonDisplay, boxDisplay) =>{
-    var box_container = document.getElementById('box_container');
-    var button_container = document.getElementById('button_container');
-    
-    box_container.style.display = boxDisplay;
-    button_container.style.display = buttonDisplay;
-}
-
 var grid_container = document.getElementById('grid-container');
 var num_button = document.querySelectorAll('.num_button');
 
@@ -35,29 +27,17 @@ num_button.forEach(bttn => {
                 var newDiv = document.createElement('div');
                 newDiv.className = 'box';
                 grid_container.appendChild(newDiv);
-            }
-            
-            grid_container.style.gridTemplate = `repeat(${buttonValue_Int}, 1fr) / repeat(${buttonValue_Int}, 1fr)`;
-            
-            let box = document.querySelectorAll('.box');
-            box.forEach(el => {
-                // ADD THE CURRENT VALUE OF THE BUTTON TO THE CURRENT ELEMENTS PRESENTED
-                // ?? USELESS FOR NOW ??
-                el.classList.add(`${buttonValue_Squared}`)
-                // ADD A RANDOM FRUIT TO EACH CELL
-                el.appendChild(randomFruitSelector())
-            });
 
-            
-            // ?? USELESS FOR NOW ??
-            // REMOVE THE PREVIOUS ELEMENTS THAT HAS AN OLD CURRENT VALUE TO MAKE ROOM FOR THE NEW CURRENT VALUE
-            if(clickedOnce == true){
-                box.forEach(el => {
-                    if(el.classList.contains(`${previousValue}`)){
-                        el.remove();
-                    }
-                });
+                var svg_img = document.createElement("img");
+                svg_img.className = "element_svg";
+                newDiv.appendChild(svg_img);
             }
+
+            var box = document.getElementsByClassName('box');
+            var svgImage = document.querySelectorAll('.element_svg');
+            addFruitToElement(box, svgImage);
+
+            grid_container.style.gridTemplate = `repeat(${buttonValue_Int}, 1fr) / repeat(${buttonValue_Int}, 1fr)`;
 
             previousValue = buttonValue_Squared;
         }
@@ -66,6 +46,15 @@ num_button.forEach(bttn => {
         clickedOnce = true;
     })
 });
+
+function elementDisplay(buttonDisplay, boxDisplay){
+    var box_container = document.getElementById('box_container');
+    var button_container = document.getElementById('button_container');
+    
+    box_container.style.display = boxDisplay;
+    button_container.style.display = buttonDisplay;
+}
+
 
 function randomFruitSelector() {
     var apple = "svgs/apple.svg";
@@ -78,14 +67,37 @@ function randomFruitSelector() {
     let list = [apple, banana, grape, kiwi, orange, tomato];
     let randomFruit = Math.floor(Math.random() * list.length);
 
-    var svg_img = document.createElement("img");
-    svg_img.className = "element_svg"; 
-    svg_img.src = `${list[randomFruit]}`; 
-
-
-    return svg_img;
+    return list[randomFruit];
 }
 
-// TODO LIST
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
-// MAKE EACH PICKED FRUIT A PAIR
+function addFruitToElement(elementClass, svg_image){
+    let lenOfElement = elementClass.length;
+    let fruits = [];
+    
+    for (let i = 0; i < lenOfElement / 2; i++) {
+        fruits.push(randomFruitSelector());
+    }
+    
+    // ARRAY PUT INTO HALF OF THE ELEMENTS
+    let firstHalf = 0;
+    while(firstHalf < lenOfElement){
+        svg_image[firstHalf].src = fruits[firstHalf];
+        firstHalf++;
+    }
+    // SHUFFLED ARRAY INTO THE LAST HALF OF THE ELEMENTS
+    let secondHalf = Math.floor(lenOfElement / 2);
+    let i = 0;
+    shuffleArray(fruits);
+    while(secondHalf < lenOfElement){
+        svg_image[secondHalf].src = fruits[i];
+        secondHalf++;
+        i++;
+    }
+}
